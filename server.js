4d05dd5a8c99cd75d0dfb39affba5b5396625697
Saksham Singh <60515013+wayiam@users.noreply.github.com>
2024-12-23 14:50:33 +0000

    updated app

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World from wayiam!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
